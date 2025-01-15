5a9d11ca0855b58d79ef5cf21c633205682c90d9
habibullah <habibullahkhandokar01@gmail.com>
2025-01-15 10:38:39 +0600

    Home Page requerment server route  complete

--- index.js
+++ index.js
@@ -22,6 +22,39 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+
+    const dataBase = client.db('PH_11_AS_server').collection('tutorials');
+
+    //!get (all/ full/ bulk) data from surver [Demo or test purpose]
+    app.get('/cards', async(req, res)=>{
+      try{
+        const cursor = dataBase.find();
+        const result = await cursor.toArray();
+        res.send(result);
+        console.log(result);
+      }
+      catch (error) {
+        console.error('Error fetching language data:', error);
+        res.status(500).send({ error: 'Failed to fetch products' });
+      }
+    })
+
+    //* get data/find-tutors based on language
+    app.get('/find-tutors/:language?', async(req, res)=>{
+      try{
+        const language = req.params.language;
+        const query = language ? { language: language } : {};
+        const result = await dataBase.find(query).toArray();
+        res.send(result);
+        console.log(result);
+      } 
+      catch (err){
+        console.error('Error from find-tutors route:', err);
+      }
+    })
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -29,7 +62,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
